8dd4e8f99d3bc47bb9b11a33563819d9cfc084f7
Max Spahn <m.spahn@tudelft.nl>
2024-10-22 20:46:45 +0200

    Activates actions.

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: "https://maxspahn.github.io",
+  base: '/tryingtailwind',
 
   integrations: [
     tailwind({

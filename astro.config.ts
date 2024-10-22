564dc2cd2dc53692a677a4b1cceb9a6af7ccdbdb
Max Spahn <m.spahn@tudelft.nl>
2024-10-22 21:56:54 +0200

    Trying out gpt solution.

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  base: process.env.BASE_URL || '/',
+  base: '/tryingtailwind/',
 
   integrations: [
     tailwind({
